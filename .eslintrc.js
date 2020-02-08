d1bb8af4a6d0bb3ccbcf29925336fedd4b7c1090
marcos.bezerra <marcosbgomes@gmail.com>
2020-02-08 10:50:22 -0300

    :wrench: Initial Prettier setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,9 +11,13 @@ module.exports = {
     env: {
         node: true,
     },
+    rules: {
+        quotes: ['error', 'single', {avoidEscape: true}],
+        'comma-dangle': ['error', 'always-multiline'],
+    },
     settings: {
         react: {
             version: '16.12.0'
         },
-    }
+    },
 };
