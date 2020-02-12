da56eee5bf8ac5f14d2d7e713f94c4e8b9cb92a4
marcos.bezerra <marcosbgomes@gmail.com>
2020-02-12 16:02:40 -0300

    :construction: Deleting unused component

--- src/tests/index.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-describe('JSX', () => {
-    it('calls React.creatElement', () => {
-        const creatElementSpy = jest.spyOn(React, 'createElement')
-        ;<h1>Hello, JSX!</h1>
-        expect(creatElementSpy).toHaveBeenCalledWith('h1', null, 'Hello, JSX!')
-    })
-})
