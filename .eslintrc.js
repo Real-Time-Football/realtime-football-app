525082938a4259f580728e60ef60d1397bbf6bb3
marcos.bezerra <marcosbgomes@gmail.com>
2020-02-12 10:39:27 -0300

    :construction: Start working on TeamScore component

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     plugins: ['react', 'prettier'],
-    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
     parserOptions: {
         ecmaVersion: 6,
         sourceType: 'module',
---
+++ src/main/TeamScore.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const TeamFlag = () => (
+    <div>
+        <image></image>
+        <label></label>
+    </div>
+)
+
+export default TeamFlag
