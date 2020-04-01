783106e03bb35dcbb9f78e5c3f4c6883d77c65df
marcos.bezerra <marcosbgomes@gmail.com>
2020-04-01 15:23:09 -0300

    :wrench: Add initial Storybook setup

---
+++ stories/0-ScoreBoard.stories.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import ScoreBoard from '../src/components/ScoreBoard'
+
+storiesOf('ScoreBoard', module).add('default', () => <ScoreBoard></ScoreBoard>)
